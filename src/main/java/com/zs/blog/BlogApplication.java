eb999d5cb0815b6ce695afc238f6e7c5dc25238d
lucky ZHOU <lucky.zhou@xplusz.com>
2016-03-22 16:22:56 +0800

    Add CORS support and add travis.yml to do ci

--- src/main/java/com/zs/blog/BlogApplication.java
+++ src/main/java/com/zs/blog/BlogApplication.java
@@ -1,7 +1,5 @@
 package com.zs.blog;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.zs.blog.core.Blog;
 import com.zs.blog.db.BlogDao;
 import com.zs.blog.resources.BlogResources;
@@ -12,7 +10,12 @@ import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+import java.util.EnumSet;
 
 public class BlogApplication extends Application<BlogConfiguration>{
 
@@ -45,8 +48,17 @@ public class BlogApplication extends Application<BlogConfiguration>{
         environment.jersey().register(new UploadResources());
 
         // Register MultiPartFeature.class to support mime type "multipart/form-data."
-        environment.jersey().getResourceConfig().register(JacksonJaxbJsonProvider.class);
         environment.jersey().getResourceConfig().register(MultiPartFeature.class);
+
+        // CORS enable
+        Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), false, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
+
     }
 
     public static void main(String[] args) throws Exception{
